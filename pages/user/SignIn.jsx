77f54d5726261b12fc596de20c465b5658dea832
SHINILSUB <deathsizes@hotmail.com>
2021-04-12 16:54:36 +0900

    add: pages-SignIn/SignUp스켈레톤추가

---
+++ pages/user/SignIn.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+import { Container } from "native-base";
+
+export default function SignIn() {
+  return (
+    <Container style={styles.container}>
+
+    </Container>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignSelf: "center",
+  },
+});
