a32849b7ebd226ffe867361237aee98a1a5599ff
Delilah1004 <pskdy8414@naver.com>
2021-04-12 14:49:00 +0900

    add: pages-article 스켈레톤 코드 추가

---
+++ pages/article/ReadArticle.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+import { Container } from "native-base";
+
+export default function ReadArticle() {
+  return <Container style={styles.container}></Container>;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignSelf: "center",
+  },
+});
