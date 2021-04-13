9d7edb735736c53e8f9a5b7f4329c8d772f445e7
Delilah1004 <pskdy8414@naver.com>
2021-04-13 15:55:08 +0900

    add: SignInput Component

---
+++ components/input/SignInput.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { View, Text, Input, Item } from 'native-base';
+
+export default function SignInput({ label, value, type, hint, setValue }) {
+  return (
+    <View style={{ width: '90%' }}>
+      <Text style={styles.label}>{label}</Text>
+      <Item regular style={styles.inputBox}>
+        <Input
+          style={value == '' ? styles.hint : styles.input}
+          // type이 패스워드면 화면상에 텍스트가 안보이게 처리하는 속성
+          secureTextEntry={type == 'password' ? true : false}
+          placeholder={hint}
+          placeholderTextColor={'#111'}
+          value={value}
+          onChangeText={(text) => {
+            setValue(text);
+          }}
+        />
+      </Item>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  label: {
+    color: '#263238',
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  inputBox: {
+    paddingLeft: 5,
+    marginBottom: 30,
+    borderRadius: 4,
+    borderColor: '#999',
+    alignSelf: 'center',
+    alignItems: 'center',
+  },
+  hint: {
+    opacity: 0.4,
+  },
+  input: {
+    opacity: 1,
+  },
+});
--- pages/user/SignUp.jsx
+++ pages/user/SignUp.jsx
@@ -1,13 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { getStatusBarHeight } from 'react-native-status-bar-height';
-import { StyleSheet } from 'react-native';
-import { Container, View, Text } from 'native-base';
+import { Dimensions, StyleSheet } from 'react-native';
+import { Container, View, Text, Header, Button } from 'native-base';
 
-export default function SignUp() {
+import { SignInput } from '../../components/input';
+
+import { MaterialIcons } from '@expo/vector-icons';
+
+const windowHeight = Dimensions.get('window').height;
+
+export default function SignUp({ navigation }) {
+  const [id, setId] = useState('');
+  const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
+
   return (
     <Container style={styles.container}>
+      {/* 헤더 */}
+      <Header style={styles.header} transparent>
+        <Button transparent onPress={() => navigation.goBack()}>
+          <MaterialIcons name="arrow-back" size={24} color="black" />
+        </Button>
+
+        <Text>회원가입</Text>
+
+        <Button transparent>
+          <MaterialIcons name="arrow-back" size={24} color="transparent" />
+        </Button>
+      </Header>
+
+      {/* Contents */}
       <View style={styles.content}>
-        <Text>SignUp Page 입니다</Text>
+        {/* 아이디 */}
+        <SignInput
+          label={'아이디'}
+          value={id}
+          type={'id'}
+          hint={'아이디를 입력하세요'}
+          setValue={setId}
+        />
+
+        {/* 비밀번호 */}
+        <SignInput
+          label={'비밀번호'}
+          value={password}
+          type={'password'}
+          hint={'비밀번호를 입력하세요'}
+          setValue={setPassword}
+        />
+
+        {/* 이름 */}
+        <SignInput
+          label={'이름'}
+          value={name}
+          type={'name'}
+          hint={'이름을 입력하세요'}
+          setValue={setName}
+        />
       </View>
     </Container>
   );
@@ -16,10 +65,17 @@ export default function SignUp() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: getStatusBarHeight(),
-    justifyContent: 'center',
+  },
+  header: {
+    width: '100%',
+    borderBottomWidth: 1.8,
+    borderBottomColor: '#D8D8D8',
+    justifyContent: 'space-between',
+    alignItems: 'center',
   },
   content: {
-    alignItems: 'center',
+    height: windowHeight - getStatusBarHeight(),
+    alignSelf: 'center',
+    justifyContent: 'center',
   },
 });
