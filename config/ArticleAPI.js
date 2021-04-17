66cc247562e4444deb0896b247e6b5a7e1474e20
Delilah1004 <pskdy8414@naver.com>
2021-04-18 01:15:04 +0900

    fix: create/update/delete Article

--- config/ArticleAPI.js
+++ config/ArticleAPI.js
@@ -8,7 +8,7 @@ const host = 'http://13.125.8.25/api';
 export async function createArticle(navigation, title, contents) {
   try {
     const token = await AsyncStorage.getItem('session');
-    const response = await axios({
+    await axios({
       method: 'post',
       url: host + '/article',
       headers: {
@@ -20,12 +20,8 @@ export async function createArticle(navigation, title, contents) {
       },
     });
 
-    if (response.data) {
-      await Alert.alert('업로드 완료!');
-      navigation.navigate('TabNavigator');
-    } else {
-      Alert.alert('업로드 실패');
-    }
+    await Alert.alert('업로드 완료!');
+    await navigation.goBack();
   } catch (err) {
     const error = err.response.data.error || err.message;
 
@@ -67,7 +63,7 @@ export async function readArticle(navigation, title, contents) {
 
     if (response.data) {
       await Alert.alert('업로드 완료!');
-      navigation.navigate('TabNavigator');
+      await navigation.goback();
     } else {
       Alert.alert('업로드 실패');
     }
@@ -75,5 +71,51 @@ export async function readArticle(navigation, title, contents) {
     const error = err.response.data.error || err.message;
 
     Alert.alert(error);
+  }
+}
+
+// 완료
+export async function updateArticle(navigation, title, contents, articleId) {
+  try {
+    const token = await AsyncStorage.getItem('session');
+    const response = await axios({
+      method: 'put',
+      url: host + '/article/' + articleId,
+      headers: {
+        Authorization: token,
+      },
+      data: {
+        title: title,
+        contents: contents,
+      },
+    });
+
+    await Alert.alert(response.data.Success);
+    await navigation.navigate('TabNavigator');
+  } catch (err) {
+    const error = err.response.data.err || err.message;
+
+    await Alert.alert(error);
+  }
+}
+
+// 완료
+export async function deleteArticle(navigation, articleId) {
+  try {
+    const token = await AsyncStorage.getItem('session');
+    const response = await axios({
+      method: 'delete',
+      url: host + '/article/' + articleId,
+      headers: {
+        Authorization: token,
+      },
+    });
+
+    await Alert.alert(response.data.Success);
+    await navigation.goBack();
+  } catch (err) {
+    const error = err.response.data.err || err.message;
+
+    await Alert.alert(error);
   }
 }
