1ecaa6606fb5ce4162daf359c1b4587265cdcd4b
Delilah1004 <pskdy8414@naver.com>
2021-04-20 03:06:05 +0900

    style: comment style 변경

--- components/item/CommentItem.jsx
+++ components/item/CommentItem.jsx
@@ -2,55 +2,49 @@ import React from 'react';
 import { StyleSheet, Text, Dimensions, TouchableOpacity } from 'react-native';
 import { Thumbnail, View } from 'native-base';
 
-const my = require('../../assets/icon.png');
+const img = require('../../assets/mask_lion.jpg');
 
 const WindowWidth = Dimensions.get('window').width;
-const ThumbSize = WindowWidth * 0.12;
+const ThumbSize = WindowWidth * 0.1;
 
 export default function CommentItem({ navigation, comment }) {
   return (
     <View style={styles.container}>
-      <View style={styles.itemHeader}>
-        {/* 댓글 작성자 이미지 */}
-        <TouchableOpacity
-          onPress={() => {
-            navigation.push('MemberInfo', comment.username);
-          }}
-        >
-          <Thumbnail style={styles.thumbnail} source={my} />
-        </TouchableOpacity>
+      {/* 댓글 작성자 이미지 */}
+      <TouchableOpacity
+        onPress={() => {
+          navigation.push('MemberInfo', comment.username);
+        }}
+      >
+        <Thumbnail style={styles.thumbnail} source={img} />
+      </TouchableOpacity>
 
-        <View style={styles.infoBox}>
-          <View style={styles.user}>
-            {/* 댓글 작성자 이름 */}
-            <Text style={styles.authorName}>
-              {comment.cmtUsername}({comment.cmtUserId})
-            </Text>
-          </View>
+      <View style={styles.infoBox}>
+        {/* 댓글 작성자 이름 */}
+        <Text style={styles.authorName}>
+          {comment.cmtUsername}({comment.cmtUserId})
+        </Text>
 
-          {/* 댓글 작성자 스택 */}
-          <Text style={styles.authorRole}>{comment.stack}</Text>
-        </View>
+        {/* 댓글 작성자 스택 */}
+        <Text style={styles.authorStack}>{comment.stack}</Text>
+
+        {/* 댓글 내용 */}
+        <Text style={styles.comment} numberOfLines={3}>
+          {comment.comments}
+        </Text>
       </View>
-
-      {/* 댓글 내용 */}
-      <Text style={styles.comment} note numberOfLines={3}>
-        {comment.comments}
-      </Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#FFF',
-    padding: 20,
-    marginVertical: 7,
-    borderBottomWidth: 1,
-    borderBottomColor: '#DBDBDB',
-  },
-  itemHeader: {
     flexDirection: 'row',
+    backgroundColor: '#FFF',
+    padding: 10,
+    marginHorizontal: 10,
+    marginVertical: 7,
+    borderRadius: 20,
   },
   thumbnail: {
     width: ThumbSize,
@@ -60,19 +54,16 @@ const styles = StyleSheet.create({
     marginLeft: 10,
     justifyContent: 'space-around',
   },
-  user: {
-    flexDirection: 'row',
-  },
   authorName: {
     fontSize: 15,
     fontWeight: 'bold',
   },
-  authorRole: {
+  authorStack: {
     color: '#999',
-    fontSize: 14,
+    marginVertical: 5,
+    fontSize: 12,
   },
   comment: {
-    marginVertical: 10,
     lineHeight: 27,
   },
 });
