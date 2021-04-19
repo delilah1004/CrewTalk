187d6165efba2d82c90d1dc8a014945b2195c7c2
Delilah1004 <pskdy8414@naver.com>
2021-04-20 03:16:59 +0900

    add: mypage

--- pages/user/MyPage.jsx
+++ pages/user/MyPage.jsx
@@ -1,29 +1,66 @@
-import React from 'react';
-import { getStatusBarHeight } from 'react-native-status-bar-height';
-import { StyleSheet, Text } from 'react-native';
-import { Container, View } from 'native-base';
+import React, { useEffect, useState } from 'react';
+import { ScrollView, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { Container, View, Header, Button, Thumbnail } from 'native-base';
 
-import { logout } from '../../config/UserAPI';
+import { AntDesign } from '@expo/vector-icons';
+
+import { getUserInfo } from '../../config/UserAPI';
+
+const img = require('../../assets/mask_lion.jpg');
 
 export default function Mypage({ navigation }) {
+  const [user, setUser] = useState('');
+
+  useEffect(() => {
+    download();
+  }, []);
+
+  const download = async () => {
+    const result = await getUserInfo();
+    setUser(result);
+  };
+
   return (
     <Container style={styles.container}>
-      <View style={styles.content}>
-        <Text style={{ fontSize: 20 }}>MyPage 입니다.</Text>
+      {/* 헤더 */}
+      <Header style={styles.header} transparent>
+        <View>
+          <Button disabled transparent>
+            <AntDesign name="setting" size={24} color="transparent" />
+          </Button>
+        </View>
 
-        {/* 로그아웃 */}
-        <Text
-          style={{
-            fontSize: 15,
-            textDecorationLine: 'underline',
-          }}
-          onPress={() => {
-            logout(navigation);
-          }}
-        >
-          로그아웃
-        </Text>
-      </View>
+        <View>
+          <Text style={styles.headerTitle}>마이페이지</Text>
+        </View>
+
+        <View>
+          <Button transparent onPress={() => navigation.push('Setting')}>
+            <AntDesign name="setting" size={24} color="black" />
+          </Button>
+        </View>
+      </Header>
+
+      {/* 바디 */}
+      <ScrollView>
+        {/* 프로필 */}
+        <View style={styles.profileBox}>
+          {/* 사용자 사진 */}
+          <TouchableOpacity>
+            <Thumbnail style={styles.thumbnail} source={img} />
+          </TouchableOpacity>
+
+          <View style={styles.profileInfo}>
+            {/* 사용자 이름 */}
+            <Text style={styles.userName}>
+              {user.name} ({user.username})
+            </Text>
+
+            {/* 사용자 Stack */}
+            <Text style={styles.userStack}>{user.stack}</Text>
+          </View>
+        </View>
+      </ScrollView>
     </Container>
   );
 }
@@ -31,12 +68,53 @@ export default function Mypage({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: getStatusBarHeight(),
+    backgroundColor: '#EEE',
+  },
+  header: {
+    backgroundColor: '#FFF',
+    borderBottomWidth: 1.5,
+    borderBottomColor: '#EEE',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  headerTitle: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+
+  // 프로필 영역
+  profileBox: {
+    backgroundColor: '#FFF',
+    flexDirection: 'row',
+    padding: 20,
+    alignItems: 'center',
+  },
+  // 프로필 정보 영역
+  profileInfo: {
+    paddingStart: 20,
     justifyContent: 'center',
   },
-  content: {
-    height: '100%',
+  // 사용자 이름
+  userName: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginVertical: 5,
+  },
+  // 사용자 Stack
+  userStack: {
+    color: '#999',
+    fontSize: 14,
+    marginVertical: 5,
+  },
+
+  // flexDirection -> row
+  flexRow: {
+    flexDirection: 'row',
+  },
+  followBox: {
+    flexDirection: 'row',
+    marginVertical: 30,
     alignItems: 'center',
-    justifyContent: 'space-around',
   },
 });
